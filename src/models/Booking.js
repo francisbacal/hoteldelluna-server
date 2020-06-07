ee3e55dc9ee086fe6c30a23fe03d2111bf2de021
Bacal Francis <francisbacal@gmail.com>
2020-06-07 16:54:00 +0800

    Booking model done

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -1,2 +1,36 @@
 import mongoose from 'mongoose';
-import {Schema} from 'mongoose'
+import {Schema} from 'mongoose';
+
+const BookingSchema = new Schema(
+    { 
+        customerEmail: 
+        {
+            type: String,
+            required: [true, 'Email required']
+        },
+        room:
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Room'
+        },
+        bookingDate:
+        {
+            start: {type: Date, required: [true, 'Date required']},
+            end: {type: Date, required: [true, 'Date required']}
+        },
+        total:
+        {
+            type: Number,
+            required: [true, 'Total price required']
+        },
+        hasEnded:
+        {
+            type: Boolean,
+            default: false
+        }
+    }
+)
+
+const Booking = mongoose.model('Booking', BookingSchema)
+
+export default Booking
